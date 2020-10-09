818b9a9ccea4905c926cd28d14e5ffbfa542e538
Manish Pathak <immanish8521@gmail.com>
2020-10-10 01:15:42 +0530

    Updates

---
+++ precache-manifest.68c9e30bc458c09d1415bbb17a030273.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911d17d5f3bfe66e379e9ed344ce7218",
+    "url": "/React-E-comm/index.html"
+  },
+  {
+    "revision": "17bdd369ad6416464341",
+    "url": "/React-E-comm/static/css/main.38054590.chunk.css"
+  },
+  {
+    "revision": "de5a655878f7789f971c",
+    "url": "/React-E-comm/static/js/2.7edd4175.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/React-E-comm/static/js/2.7edd4175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17bdd369ad6416464341",
+    "url": "/React-E-comm/static/js/main.93f24ffc.chunk.js"
+  },
+  {
+    "revision": "ac72f7701701e295d382",
+    "url": "/React-E-comm/static/js/runtime-main.04a6fbed.js"
+  },
+  {
+    "revision": "1190aa98ff47e158700b9c6854a7a7f2",
+    "url": "/React-E-comm/static/media/logo.1190aa98.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/React-E-comm/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
